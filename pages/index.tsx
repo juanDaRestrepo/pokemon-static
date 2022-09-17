f0a592275e1c918f2711eb8852c1c391080cb604
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-09-17 12:47:13 -0500

    Creating _document.tsx for correct styling in different browsers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
+
+
 
 const HomePage: NextPage = () => {
   return (
    <>
-    <h1>Hello world!</h1>
+    <Button color="gradient">
+      Hello world!
+    </Button>
    </>
   )
 }
