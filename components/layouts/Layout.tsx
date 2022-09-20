81136fd2b09c7d40f6c5b0a2b35093d7334ef3f5
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-09-19 22:12:39 -0500

    Made interface for api response

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,7 @@
-import Head from "next/head"
 import { FC, PropsWithChildren } from "react"
+
+import Head from "next/head"
+import { Navbar } from '../ui';
 
 interface Props{
     title: string;
@@ -15,9 +17,11 @@ export const Layout: FC<PropsWithChildren<Props>>= ({ children, title }) => {
             <meta name="keywords" content={`${title}, pokemon, pokedex`}/>
         </Head>
 
-        {/* Navbar */}
+        <Navbar/>
 
-        <main>
+        <main style={{
+          padding:'0px 20px'
+        }}>
             { children }
         </main>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,34 @@
-import { Button } from '@nextui-org/react'
 
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
+import { pokeApi } from '../api';
 import { Layout } from '../components/layouts';
+import { PokemonListsResponse } from '../interfaces/pokemon-list';
 
 
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
+  
   return (
 
     <Layout title='Pokemons list'>
-      <Button color="gradient">
-        Hello world!
-      </Button>
+      
     </Layout>
    
   )
+}
+
+//This function is for static generation and can be only use in pages
+//is executes when aplication is built an executes only one time 
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  
+  const { data } = await pokeApi.get<PokemonListsResponse>('/pokemon?limit=151');
+  
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
 }
 
 export default HomePage
